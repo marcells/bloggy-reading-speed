ff13baa89918d94aa3d886a81fd3e56a807b1489
Marcell Spies <marcells@gmx.de>
2014-05-31 19:18:42 +0200

    Added counting stuff.

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 'use strict';
 
-var minidom = require('minidom'),
-    htmlencode = require('node-htmlencode'),
-    makeSummary = function (content, maxLength, prefix, suffix) {
-        var summary = content
-            .substr(0, content.lastIndexOf(' ', maxLength));
+var minidom = require('minidom');
 
-        return prefix + htmlencode.htmlEncode(summary).replace(/&#10;/g, '<br />') + suffix;
-    },
-    makeMetaDescription = function (content) {
-        var minimumMetaDescriptionLengthToNextSpace = 180; // http://webdesign.about.com/od/metatags/qt/meta_descriptio.htm
-
-        return content
-                .replace(/\"/g, '\'')
-                .substr(0, content.indexOf(' ', minimumMetaDescriptionLengthToNextSpace))
-                .concat('...');
-    };
-
-exports.maxLength = 300;
-exports.prefix = '<p>';
-exports.suffix = ' ...<p>';
+exports.wordsPerMinute = 200;
+exports.countWords = function (content) {
+    return content.split(/\b\S+\b/g).length - 1;
+};
 
 exports.init = function (engine) {
     var original = engine.loadContent;
@@ -27,10 +13,11 @@ exports.init = function (engine) {
     engine.loadContent = function (entry, content, callback) {
         original(entry, content, function (transformedContent) {
             var dom = minidom('<div>' + transformedContent + '</div>'),
-                text = dom.children[0].textContent;
+                text = dom.children[0].textContent,
+                numberOfWords = exports.countWords(text);
 
-            entry.summary = makeSummary(text, exports.maxLength, exports.prefix, exports.suffix);
-            entry.metaDescription = makeMetaDescription(text);
+            entry.numberOfWords = numberOfWords;
+            entry.readingSpeed = Math.round(numberOfWords / exports.wordsPerMinute * 60);
 
             callback(transformedContent);
         });
